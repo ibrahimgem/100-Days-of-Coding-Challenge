04086fd1a43ecb8ba395062c69876205272503d5
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-24 19:21:09 +0500

    Day 10 Challenge Task 2

---
+++ Day 10 Challenge/29. Favorite Fruit/main.js
@@ -0,0 +1,9 @@
+"use strict";
+// Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+let favoriteFruits = ["Banana", "Mango", "Apple", "Grapes"];
+if (favoriteFruits.includes("Apple")) {
+    console.log(`This fruit is your favorite.`);
+}
+else {
+    console.log(`This fruit is not your favorite.`);
+}
---
+++ Day 10 Challenge/29. Favorite Fruit/main.ts
@@ -0,0 +1,10 @@
+// Question 29: Favorite Fruit: Create an array for your favorite fruits and check if certain fruits are included.
+
+let favoriteFruits: Array <String> = ["Banana", "Mango", "Apple", "Grapes"];
+
+if (favoriteFruits.includes("Apple")) {
+    console.log(`This fruit is your favorite.`);
+} else {
+    console.log(`This fruit is not your favorite.`);
+    
+}
