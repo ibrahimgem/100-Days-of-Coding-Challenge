955af075dd54707a0d2ef176929f4df81e2e50fc
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-24 17:30:10 +0500

    Day 9 Challenge

---
+++ Day 9 Challenge/27. Alien Colors #3/main.ts
@@ -0,0 +1,12 @@
+// Question 27: Alien Colors #3: Convert your if-else chain to handle three colors: green, yellow, red.
+
+let alien_color = "red";
+if (alien_color == "green") {
+    console.log(`You just earned 5 points.`);
+} else if (alien_color == "yellow") {
+    console.log(`You earned 10 points.`)
+} else if (alien_color == "red") {
+    console.log(`You earned 15 points.`)
+}
+
+
