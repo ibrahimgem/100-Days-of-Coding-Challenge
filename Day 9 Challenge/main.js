79f3ea3436c4cd0d043a194add71490b4bf2c5e3
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-24 16:57:07 +0500

    Day 9 Commit

--- Day 9 Challenge/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-// Question 26: Alien Colors #2: Choose a color for an alien, then write an if-else chain.
-let alien_color = "red";
-if (alien_color == "green") {
-    console.log(`You just earned 5 points for shooting the alien`);
-}
-else {
-    console.log(`Ops, You lost.`);
-}
