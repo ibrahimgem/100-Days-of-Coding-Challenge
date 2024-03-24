ed5541b5811956ce8622cd1175788ad549419ac0
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-24 17:15:41 +0500

    Day 9 Challenge

---
+++ Day 9 Challenge/25. Alien Colors #1/main.js
@@ -0,0 +1,6 @@
+"use strict";
+// Question 25: Alien Colors #1: Imagine an alien was just shot down in a game. Assign 'green', 'yellow', or 'red' to a variable called alien_color.
+let alien_color = "red";
+if (alien_color == "red") {
+    console.log(`You just earned 5 points`);
+}
