dc33100b20c8a37ae1c209634e8dbb2a4cbf5675
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-25 18:24:51 +0500

    Day 12 Challenge

---
+++ Day 12 Challenge/35. Animals/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 35: Animals: Think of at least three different animals that have a common characteristic. Store the names of these animals in a list, and then use a for loop to print out the name of each animal. • Modify your program to print a statement about each animal, such as A dog would make a great pet. • Add a line at the end of your program stating what these animals have in common. You could print a sentence such as Any of these animals would make a great pet!
+//Declaring array for three animals and printing each of them with for loop.
+let animals = ['Dog', 'Cat', 'Lion'];
+animals.forEach(animal => {
+    console.log(animal);
+});
+// Printing each of animal with sentences through for loop.
+animals.forEach(animal => {
+    console.log(`A ${animal} would make a great pet.`);
+});
+console.log(`These animals would be very friendly.`);
