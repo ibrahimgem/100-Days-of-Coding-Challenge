c9ba3d5e5f23f19ff5338622712977d855480446
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-03-25 15:47:08 +0500

    Day 11 Challenge Commit

---
+++ Day 11 Challenge/33. Ordinal Numbers/main.ts
@@ -0,0 +1,15 @@
+// Question 33: Ordinal Numbers: Display numbers with their ordinal suffixes.
+
+let numbers: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+numbers.forEach(number => {
+    let suffix = "th"
+    if (number === 1) {
+        suffix = "st"
+    } else if (number === 2) {
+        suffix = "nd"
+    } else if (number === 3) {
+        suffix = "rd"
+    }
+    console.log(`${number}${suffix}`);
+});
