80a2c6dbea4325c70e22e5e2057a3f8afd82f4a5
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-09 18:58:50 +0500

    Task 60 Update

--- Day 20 Challenge/60. Self-Running User Profile/main.js
+++ Day 20 Challenge/60. Self-Running User Profile/main.js
@@ -1,18 +1,6 @@
+"use strict";
 // Question 60: Self-Running User Profile: Create a quick, self-setup profile for a user that can tell you the user's name and age.
-// function selfProfileSetup (username: string, age: number) {
-//     return console.log(`Username: ${username}, Age: ${age} years old.`)
-// }
-// selfProfileSetup("Ibrahim", 27);
-var userProfile = (function () {
-    // The user's details are kept inside
-    var name = "John";
-    var age = 30;
-    // This part shares the user's details
-    return {
-        displayInfo: function () {
-            console.log("Name: ".concat(name, ", Age: ").concat(age));
-        }
-    };
-})();
-// Asking the profile to tell us about the user
-userProfile.displayInfo(); //
+function selfProfileSetup(username, age) {
+    return console.log(`Username: ${username}, Age: ${age} years old.`);
+}
+selfProfileSetup("Ibrahim", 27);
