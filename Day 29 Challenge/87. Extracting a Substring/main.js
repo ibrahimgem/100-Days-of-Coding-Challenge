0a8087a59c6329dec27e9fecf4fd9eb096b1b0a5
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-14 17:15:06 +0500

    Day 29

---
+++ Day 29 Challenge/87. Extracting a Substring/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 87: Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+// Explain & TIP: .substring() can be used to get a specific part of a string, by specifying the start and end positions.
+function extractSubstring(text) {
+    console.log(text.substring(0, 10));
+}
+extractSubstring("The quick brown fox jumps over the lazy dog.");
---
+++ Day 29 Challenge/87. Extracting a Substring/main.ts
@@ -0,0 +1,8 @@
+// Question 87: Extracting a Substring: Demonstrate how to extract the first 10 characters from a string.
+
+// Explain & TIP: .substring() can be used to get a specific part of a string, by specifying the start and end positions.
+
+function extractSubstring(text:string) {
+    console.log(text.substring(0, 10))
+}
+extractSubstring("The quick brown fox jumps over the lazy dog.");
