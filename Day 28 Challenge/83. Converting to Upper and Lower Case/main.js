177db86cbd49facfced729085d89072d78dc38bd
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-14 16:46:51 +0500

    day 28

---
+++ Day 28 Challenge/83. Converting to Upper and Lower Case/main.js
@@ -0,0 +1,7 @@
+// Question 83: Converting to Upper and Lower Case: Create a function that takes a string, converts it to uppercase, then to lowercase, and logs both.
+// Explain & TIP: The .toUpperCase() and .toLowerCase() methods allow you to change the case of all letters in a string, useful for formatting or comparing strings.
+function upperAndLowerCase(text) {
+    console.log(text.toLowerCase(), text.toUpperCase());
+}
+;
+upperAndLowerCase("The Sentence.");
