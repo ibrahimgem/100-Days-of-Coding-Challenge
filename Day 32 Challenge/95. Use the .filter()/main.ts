ed49898d020f878208346264d9833f14d914ac21
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-15 16:13:30 +0500

    Day 32

---
+++ Day 32 Challenge/95. Use the .filter()/main.ts
@@ -0,0 +1,9 @@
+// Question 95: Write a function that uses the .filter() method to return an array of numbers greater than 10.
+
+// Explain & TIP: The .filter() method creates a new array with all elements that pass the test implemented by the provided function.
+
+function arrayOfGreaterThan10(nums:number[]): number[] {
+    return nums.filter(nums => nums > 10);
+}
+let nums: number[] = [10, 22, 5, 7, 24, 11, 39];
+console.log(arrayOfGreaterThan10(nums));
