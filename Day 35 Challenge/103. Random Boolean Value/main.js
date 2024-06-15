5b911886a1e97d66e5c851ab21c2637707e0cfa0
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-15 17:43:39 +0500

    Day 35

---
+++ Day 35 Challenge/103. Random Boolean Value/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 103: Write a function that returns a random boolean value, true or false.
+// Explain & TIP: Generating a random boolean is a simple but useful trick. You can use Math.random() and check if it's above or below 0.5 to decide between true or false.
+function randomBoolean() {
+    return Math.random() >= 0.5;
+}
+console.log(randomBoolean());
---
+++ Day 35 Challenge/104. Random Hexdecimal Color Code/main.ts
@@ -0,0 +1,16 @@
+// Question 104: Create a function that generates a random hexadecimal color code.
+
+// Explain & TIP: Hexadecimal color codes are strings that start with # followed by 6 hexadecimal digits. You can generate each digit randomly and concatenate them into a full color code.
+
+function getRandomHexColor () {
+    let randomInt = () => Math.floor(Math.random() * 256);
+    let toHex = (int: any) => int.toString(16).padStart(2, '0');
+
+    let red = toHex(randomInt());
+    let green = toHex(randomInt());
+    let blue = toHex(randomInt());
+    
+    return `#${red}${green}${blue}`
+}
+
+console.log(getRandomHexColor());
