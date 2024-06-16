cf78baa2ba17683c04a09357c0d63342a33db17b
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-16 16:31:11 +0500

    Day 37 Commit

---
+++ Day 37 Challenge/110. Grading System/main.ts
@@ -0,0 +1,29 @@
+// Question 110: Create a function that assigns a grade (A, B, C, D, F) based on a student's score.
+
+// Explain & TIP: A common grading system assigns letter grades based on score ranges. You can use an if-else-if chain to determine the grade.
+
+function gradingSystem (score: number) {
+    if (score >= 80 && score <= 100) {
+        console.log("Congratulations! You have secured 'A+' Grade")
+    } else if (score >= 70 && score <= 79) {
+        console.log("Congratulations! You have secured 'A' Grade")
+    } else if (score >= 60 && score <= 69) {
+        console.log("Congratulations! You have secured 'B' Grade")
+    } else if (score >= 50 && score <= 59) {
+        console.log("Good! You have secured 'C' Grade")
+    } else if (score >= 40 && score <= 49) {
+        console.log("Need Improvements, You have secured 'D' Grade")
+    } else if (score >= 30 && score <= 39) {
+        console.log("Need Improvements, You have secured 'E' Grade")
+    } else {
+        console.log("You have failed.")
+    }
+}
+
+(gradingSystem(85));
+(gradingSystem(73));
+(gradingSystem(62));
+(gradingSystem(52));
+(gradingSystem(42));
+(gradingSystem(32));
+(gradingSystem(22));
---
+++ Day 37 Challenge/111. Age Group/main.ts
@@ -0,0 +1,17 @@
+// Question 111: Use an if-else-if chain to categorize a person's age group (child, teenager, adult).
+
+// Explain & TIP: Age groups can typically be categorized by specific ranges. For instance, you might consider anyone under 13 a child, between 13 and 19 a teenager, and 20 or older an adult.
+
+function ageGroup(age: number) {
+    if (age < 13) {
+        console.log("You are child.")
+    } else if (age >= 13 && age <= 19) {
+        console.log("You are teenager.");
+    } else {
+        console.log("You are adult.");
+    }
+}
+
+ageGroup(8);
+ageGroup(22);
+ageGroup(13);
