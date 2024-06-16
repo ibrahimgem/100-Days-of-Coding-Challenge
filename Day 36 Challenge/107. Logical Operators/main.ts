acafd85e517175c142faa3bae5b01ce11ffb1073
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-16 15:54:05 +0500

    Day 36 Commit

---
+++ Day 36 Challenge/107. Logical Operators/main.ts
@@ -0,0 +1,11 @@
+// Question 107: Write a condition using logical operators that checks if a number is divisible by both 2 and 3.
+
+// Explain & TIP: Checking for divisibility involves using the modulo operator (%) to see if there's any remainder. A number divisible by both 2 and 3 without a remainder is also divisible by 6.
+
+function logicalOperators (num: number) {
+    return num % 2 === 0 && num % 3 === 0
+}
+console.log(logicalOperators(5));
+console.log(logicalOperators(6));
+console.log(logicalOperators(7));
+
---
+++ Day 36 Challenge/108. Validating Strings Identical/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 108: Compare two strings to check if they are identical, ignoring case sensitivity.
+// Explain & TIP: To compare strings without considering their case (uppercase/lowercase), you can convert both strings to the same case before comparing.
+function twoStrings(str1, str2) {
+    return str1.toLowerCase() === str2.toLowerCase();
+}
+console.log(twoStrings("Karachi", "KARACHI"));
