c3b2dfbe2913fcea011d436db2a8d02db1091d3f
Muhammad Ibrahim <iby.khan7@gmail.com>
2024-06-16 18:09:43 +0500

    Day 40 Commit

---
+++ Day 40 Challenge/120. For of Loop/main.js
@@ -0,0 +1,7 @@
+"use strict";
+// Question 120: Use a for...of loop to iterate through an array of your favorite movies and log each one.
+// Explain & TIP: The for...of loop is a modern loop introduced in ES6, designed to iterate over iterable objects like arrays, strings, Maps, NodeLists, and more, making it very readable and easy to use.
+let favMovies = ["3 Idiots", "PK", "Avengers"];
+for (const iterator of favMovies) {
+    console.log(iterator);
+}
