7ad7784751f5b1e90cd4d806fa0cf5c0692cc546
ibrahim <iby.khan7@gmail.com>
2024-06-17 17:53:52 +0500

    Day 46

---
+++ Day 46 Challenge/136. Use console.log() to debug and track the value of a variable inside a loop/main.ts
@@ -0,0 +1,7 @@
+// Question 136 Use console.log() to debug and track the value of a variable inside a loop.
+
+// Explain & TIP: console.log() is an invaluable tool for developers to monitor the values of variables, the flow of execution, and to understand the state of their program at various points in time, especially within loops where values change with each iteration.
+ 
+for (let i = 0; i <= 5; i++) {
+    console.log(`Iteration ${i} of value i`);
+}
---
+++ Day 46 Challenge/137. Implement a try-catch block to handle potential errors in a block of code/main.ts
@@ -0,0 +1,20 @@
+// Question 137 Implement a try-catch block to handle potential errors in a block of code.
+
+// Explain & TIP: The try-catch statement marks a block of statements to try, and specifies a response, should an exception be thrown. It's a critical feature for robust error handling in JavaScript.
+
+function testFunction () {
+    if (Math.random() > 0.5) {
+        throw new Error("An error occurred.")
+} else {
+    return "Success";
+}
+}
+try {
+    let result = testFunction();
+    console.log(result);
+    
+} catch (error) {
+    console.error("There is an error.");
+} finally {
+    console.log("This code is runs regardless error occurred or not.");   
+}
