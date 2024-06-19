132ceadd64a8168421455cd0cb09820094821cdd
ibrahim <iby.khan7@gmail.com>
2024-06-20 01:55:08 +0500

    Day 50 Commit

---
+++ Day 50 Challenge/150. Asynchronous Callback vs Synchrounous Code/main.js
@@ -0,0 +1,12 @@
+"use strict";
+// Question 150: Describe how asynchronous callbacks differ from synchronous code execution.
+// Explain & TIP: Asynchronous callbacks allow JavaScript to perform long network requests, file operations, or set timers without blocking the main thread, enabling the continuation of code execution. Synchronous code, in contrast, runs sequentially, blocking further execution until the current operation completes.
+console.log('Start');
+// Synchronous task
+for (let i = 0; i < 1000000000; i++) { } // Simulates a long-running task
+console.log('Synchronous task finished');
+// Asynchronous task
+setTimeout(() => {
+    console.log('Asynchronous callback');
+}, 1000);
+console.log('End');
